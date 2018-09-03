e8e1dc7bb5c1b0b831dbd40ebf7e0ee3c6d721a3
Jon Nunez Alvarez <jon.nunez@rewe-digital.com>
2018-09-03 14:48:17 +0200

    [styling] add lightweight footer

--- src/App.js
+++ src/App.js
@@ -87,6 +87,34 @@ class App extends Component {
             </div>
           </div>
         </main>
+
+        <footer className="footer">
+            <div className="footer-content-box">
+              <h2 className="footer-title">Brauchen Sie Hilfe?</h2>
+              <h3 className="footer-subtitle">Wir helfen Ihnen gerne.</h3>
+            </div>
+            {/*<div className="footer-content-box">*/}
+              {/*<h4 className="footer-awards-title">Siegel und Auszeichnungen</h4>*/}
+              {/*<img alt="logo ehi" src="./src/img/logo-ehi.png" />*/}
+              {/*<p className="footer-price-explanation">Alle Preise in Euro (€) inkl. gesetzlicher Mehrwertsteuer, zuzüglich Versandkosten, Pfand und optionaler Servicegebühren.</p>*/}
+            {/*</div>*/}
+            <div className="checkout-footer-nav-container">
+                <div className="checkout-footer-nav-inner">
+                    <a target="_blank" rel="noopener noreferrer" className="checkout-footer-nav-item" href="https://www.rewe.de/service/agb/" title="Allgemeine Geschäftsbedingungen">
+                        AGB
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer" className="checkout-footer-nav-item" href="https://www.rewe.de/service/impressum/" title="Impressum">
+                        Impressum
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer" className="checkout-footer-nav-item" href="https://www.rewe.de/service/datenschutz/" title="Datenschutz">
+                        Datenschutz
+                    </a>
+                    <a target="_blank" rel="noopener noreferrer" className="checkout-footer-nav-item" href="https://www.rewe.de/service/nutzungsrichtlinien/" title="Nutzungsrichtlinien">
+                        Nutzungsrichtlinien
+                    </a>
+                </div>
+            </div>
+        </footer>
       </div>
     );
   }
