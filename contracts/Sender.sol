c38874513aa633000027a3b6011f0dc67f0aef0b
Kai Tait <kai.tait@rewe-digital.com>
2018-09-04 11:36:14 +0200

    Updated Sender contract and App.js attempting to use the contract to send eth to another address

--- contracts/Sender.sol
+++ contracts/Sender.sol
@@ -4,7 +4,7 @@ contract Sender {
     uint256 public balance;
 
     function send(address _receiver) payable {
-        msg.sender.transfer(msg.value);
+        _receiver.transfer(msg.value);
     }
 
     function getBalance() public returns (uint) {
