1ce00bfa0ab25bce2197b32923f22580533e3ac6
Jon Nunez Alvarez <jon.nunez@rewe-digital.com>
2018-09-04 14:48:23 +0200

    [js] handling changes into inputs to resolve total amount to pay

--- src/lineitem/lineitem.js
+++ src/lineitem/lineitem.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const LineItem = ({ imgUrl, description, details, price, amount = 1, changeHandler}) => (
-    <article className="line-item">
-        <img className="line-item-image" alt="product" src={imgUrl}/>
-        <div className="line-item-info">
-            <p className="line-item-description">{description}</p>
-            <p className="line-item-details">{details}</p>
-        </div>
-        <p className="line-item-price">{price + '€'}</p>
-        <input className="line-item-price-input" type="text" name="price" maxLength="2" value={amount}
-               onChange={changeHandler}/>
-    </article>
-);
+class LineItem extends Component {
+    constructor(props) {
+        super(props);
+        console.log("this.props.price * this.props.amount: " + this.props.price * this.props.amount);
+
+        this.state = {
+            currentValue: this.props.amount,
+            collectorAmount: this.props.price * this.props.amount
+        }
+
+        this.changeIt = this.changeIt.bind(this);
+    }
+
+    changeIt(e) {
+        this.setState({
+            collectorAmount: parseInt(e.target.value) * parseInt(this.props.price)
+        });
+    }
+
+    render() {
+        return (
+            <article className="line-item">
+                <img className="line-item-image" alt="product" src={this.props.imgUrl}/>
+                <div className="line-item-info">
+                    <p className="line-item-description">{this.props.description}</p>
+                    <p className="line-item-details">{this.props.details}</p>
+                </div>
+                <p className="line-item-price">{this.props.price + '€'}</p>
+                <input className="line-item-price-input" type="text" name="price" maxLength="2" defaultValue={this.state.currentValue}
+                       onChange={this.changeIt} onBlur={this.props.changeHandler}/>
+                <input className="price-collector" type="hidden" value={this.state.collectorAmount}
+                       />
+            </article>
+        );
+    }
+}
+
+LineItem.defaultProps = {
+    amount: 1
+}
 
 export default LineItem;
