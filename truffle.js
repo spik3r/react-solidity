c27544b6ca463c9ce6eccd82f320a217032934cc
Kai Tait <kai.tait@rewe-digital.com>
2018-09-04 14:37:42 +0200

    finally got the sending to work

--- truffle.js
+++ truffle.js
@@ -2,8 +2,18 @@ module.exports = {
     networks: {
         development: {
             host: "localhost",
-            port: 9545,
-            network_id: "*" // Match any network id
+            port: 8545,
+            network_id: "5777" // Match any network id
+        },
+        ropsten:  {
+            network_id: 3,
+            host: "localhost",
+            port:  8545,
+            gas:   2900000
         }
+    },
+    rpc: {
+        host: 'localhost',
+        post:8080
     }
 };
